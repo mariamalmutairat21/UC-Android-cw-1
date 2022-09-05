98216659aed0baad7907ee238012e1eb748de2ab
mariamalmutairat <112577988+mariamalmutairat@users.noreply.github.com>
2022-09-05 20:04:48 +0300

    day1

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+/* Online Java Compiler and Editor */ public class HelloWorld{ public static void main(String []args){ int age =28; String name ="Mariam";
+    System.out.println("hello , my name is " + name+ " and i'm "+ age + " years old "); double Hight=160;
+    if (Hight>=160){ System.out.println("your hight is accepted");
+    }else { System.out.println("your too short"); }
+
+}
+}
